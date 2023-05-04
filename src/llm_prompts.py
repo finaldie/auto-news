fb2170e639b62939a8b975e6bd7a75031ebabcf0
Yuzhang Hu <hyzwowtools@gmail.com>
2023-05-04 23:12:58 +0000

    [af_save] push ranked tweets to ToRead database

--- src/llm_prompts.py
+++ src/llm_prompts.py
@@ -1,5 +1,16 @@
 # All LLM prompts put here
 
 LLM_PROMPT_CATEGORY_AND_RANKING_TPL = """
-You are a content review expert, you can analyze how many topics in a content, and be able to calculate a quality score of them (range 0 to 1). I’ll give u a content, and you will output a table with each topic, category and its score, and a overall score of the entire content. You should only respond in JSON format and ensure the response can be parsed by Python json.loads. The content is {content}
+You are a content review expert, you can analyze how many topics in a content, and be able to calculate a quality score of them (range 0 to 1).
+
+I’ll give u a content, and you will output a response with each topic, category and its score, and a overall score of the entire content.
+
+You should only respond in JSON format as described below
+Reponse format:
+{
+  "topics": [ an array of dicts, each dict has 3 fields "topic", "category" and "score"],
+  "overall_score": 0.9
+}
+
+Ensure the response can be parsed by Python json.loads. The content is {content}
 """
