674ff1f8407f8973e3b373e61e4bc21c4c83b772
Yuzhang Hu <hyzwowtools@gmail.com>
2023-11-20 10:51:07 +0000

    split data collector and article editor

--- src/llm_autogen.py
+++ src/llm_autogen.py
@@ -1,5 +1,7 @@
 import os
 import time
+import json
+import datetime
 
 from itertools import islice
 from duckduckgo_search import DDGS
@@ -10,6 +12,7 @@ from llm_agent import (
 )
 
 import utils
+import arxiv
 
 
 #######################################################################
@@ -68,6 +71,49 @@ def scrape(url: str):
     return summary
 
 
+def arxiv_search(query: str, days_ago=30, max_results=10):
+    start_time = (datetime.datetime.now() - datetime.timedelta(days=days_ago)).strftime('%Y%m%d%H%M%S')
+    print(f"[arxiv_search] query: {query}, days_ago: {days_ago}, start_time: {start_time}, max_results: {max_results}")
+
+    # Search for papers
+    results = arxiv.Search(
+        query=query,
+        max_results=max_results,
+        sort_by=arxiv.SortCriterion.Relevance,
+        sort_order=arxiv.SortOrder.Descending
+    )
+
+    json_res = {}
+
+    for result in arxiv.Client().results(results):
+        print(f"processed arxiv result: {result}, published time: {result.published.strftime('%Y%m%d%H%M%S')}, start_time: {start_time}")
+        # Check if the paper was published >= start_time
+        if result.published.strftime('%Y%m%d%H%M%S') >= start_time:
+            print(f"| {result.title} | {', '.join(author.name for author in result.authors)} | {result.summary} | {result.entry_id} |")
+
+            json_res[result.title] = {
+                "authors": ', '.join(author.name for author in result.authors),
+                "summary": result.summary,
+                "URL": result.entry_id,
+            }
+
+    return json.dumps(json_res, ensure_ascii=False, indent=4)
+
+
+def write_to_file(text: str, filename: str, work_dir: str):
+    work_dir = work_dir or os.getenv("AN_CURRENT_WORKDIR", "./")
+    filename = filename or "output.txt"
+    filepath = f"{work_dir}/{filename}"
+
+    print(f"[write_to_file] filename: {filename}, work_dir: {work_dir}, filepath: {filepath}, text: {text}")
+
+    f = open(filepath, "w+")
+    f.write(text)
+    f.close()
+
+    return f"{text}\n\nTERMINATE {filename}"
+
+
 #######################################################################
 # Agents
 #######################################################################
@@ -86,7 +132,7 @@ class LLMAgentAutoGen:
 
         print(f"[LLMAgentAutoGen] Initialize GPT4 model_name: {_gpt4_model_name}, api_version: {_gpt4_api_version}")
 
-        _gpt3_model_name = os.getenv("AUTOGEN_GPT3_MODEL", "gpt-4-1106-preview")
+        _gpt3_model_name = os.getenv("AUTOGEN_GPT3_MODEL", "gpt-3.5-turbo-1106")
         _gpt3_api_version = os.getenv("AUTOGEN_GPT3_API_VERSION", "2023-08-01-preview")
         _gpt3_api_key = os.getenv("AUTOGEN_GPT3_API_KEY", "")
 
@@ -96,24 +142,8 @@ class LLMAgentAutoGen:
             "api_key": _gpt3_api_key,
         }]
 
-        print(f"[LLMAgentAutoGen] Initialize GPT3 model_name: {_gpt3_model_name}, api_version: {_gpt3_api_version}")
-
-        self.llm_config_gpt4 = {
-            "timeout": 600,
-            "cache_seed": 42,
-            "temperature": 0,
-            "config_list": self.gpt4_config_list,
-        }
-
-        self.llm_config_gpt3 = {
-            "timeout": 600,
-            "cache_seed": 42,
-            "temperature": 0,
-            "config_list": self.gpt3_config_list,
-        }
-
         # functions
-        self.functions = [
+        self.functions_collection = [
             {
                 "name": "search",
                 "description": "google search for relevant information",
@@ -143,7 +173,79 @@ class LLMAgentAutoGen:
                     "required": ["url"],
                 },
             },
+
+            {
+                "name": "arxiv",
+                "description": "arxiv search for relevant papers",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                            "type": "string",
+                            "description": "Arxiv search query",
+                        }
+                    },
+                    "required": ["query"],
+                },
+            },
         ]
+
+        self.functions_pub = [
+            {
+                "name": "write_to_file",
+                "description": "write text to file",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "text": {
+                            "type": "string",
+                            "description": "The text to write to file",
+                        },
+                        "filename": {
+                            "type": "string",
+                            "description": "The filename to save the text",
+                        },
+                    },
+                    "required": ["text", "filename"],
+                },
+            },
+        ]
+
+        print(f"[LLMAgentAutoGen] Initialize GPT3 model_name: {_gpt3_model_name}, api_version: {_gpt3_api_version}")
+
+        self.llm_config_gpt4 = {
+            "timeout": 120,
+            "max_retries": 2,
+            "cache_seed": 42,
+            "temperature": 0,
+            "config_list": self.gpt4_config_list,
+        }
+
+        self.llm_config_gpt3 = {
+            "timeout": 120,
+            "max_retries": 2,
+            "cache_seed": 42,
+            "temperature": 0,
+            "config_list": self.gpt3_config_list,
+        }
+
+        self.llm_config_gpt3_pub = {
+            "timeout": 120,
+            "max_retries": 2,
+            "cache_seed": 42,
+            "temperature": 0,
+            "config_list": self.gpt3_config_list,
+            "functions": self.functions_pub,
+        }
+
+        self.llm_config_gpt3_collection = {
+            "timeout": 120,
+            "max_retries": 2,
+            "cache_seed": 42,
+            "temperature": 0,
+            "config_list": self.gpt3_config_list,
+            "functions": self.functions_collection,
+        }
 
         # agents
         self.agent_planner = autogen.AssistantAgent(
@@ -154,81 +256,32 @@ class LLMAgentAutoGen:
 
         self.agent_collector = autogen.AssistantAgent(
             name="Collector",
-            system_message="Research for the given query, collect as many information as possible, and generate detailed research results with loads of technique details with all reference links attached; Add TERMINATE to the end of the research report;",
-            llm_config=self.llm_config_gpt3,
-        )
-
-        self.agent_scientist = autogen.AssistantAgent(
-            name="Scientist",
-            system_message="",
+            system_message="Information Collector. For the given query, collect as much information as possible. You can get the data from the web search or Arxiv, then scrape the content; Add TERMINATE to the end of the research report.",
             llm_config=self.llm_config_gpt3,
         )
 
         self.agent_editor = autogen.AssistantAgent(
             name="Editor",
-            system_message="You are a senior editor of an AI blogger, you will define the structure of a short blog post based on material provided by the researcher, and give it to the writer to write the blog post",
+            system_message="You are a senior editor. You will define the structure based on user's query and material provided, and give it to the Writer to write the article.",
             llm_config=self.llm_config_gpt3,
         )
 
         self.agent_writer = autogen.AssistantAgent(
             name="Writer",
-            system_message="You are a professional blogger, you will write a short blog post based on the structured provided by the editor, and feedback from reviewer; After 2 rounds of content iteration, add TERMINATE to the end of the message",
+            system_message="You are a professional article writer, you will write an article with in-depth insights based on the structure provided by the editor and the feedback from the Reviewer.",
             llm_config=self.llm_config_gpt3,
         )
 
         self.agent_reviewer = autogen.AssistantAgent(
             name="Reviewer",
-            system_message="You are a world class hash tech blog content critic, you will review & critic the written blog and provide feedback to writer. After 2 rounds of content iteration, add TERMINATE to the end of the message",
+            system_message="You are a world-class tech article critic, you will review and critique the written article and provide feedback to Writer. After 2 rounds of reviewihng iteration with the Writer, pass the article to the Publisher.",
             llm_config=self.llm_config_gpt3,
         )
 
-        self.agent_user_proxy = autogen.UserProxyAgent(
-            name="UserProxy",
-            is_termination_msg=lambda x: x.get("content", "") and x.get("content", "").rstrip().endswith("TERMINATE"),
-            human_input_model="NEVER",
-            max_consecutive_auto_reply=10,
-            code_execution_config=False,
-            llm_config=self.llm_config_gpt3,
-            system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin",
-        )
-
-        self.agent_executor = autogen.UserProxyAgent(
-            name="Executor",
-            system_message="Executor. Execute the task by provided funtions and report the result.",
-            human_input_model="NEVER",
-            code_execution_config={
-                "last_n_messages": 2,
-                "work_dir": "data/autogen"
-            },
-        )
-
-        self.agent_executor.register_function(
-            function_map={
-                "search": search,
-                "scrape": scrape,
-            }
-        )
-
-        # create group
-        self.agents = [
-            self.agent_editor,
-            self.agent_writer,
-            self.agent_planner,
-            self.agent_scientist,
-            self.agent_collector,
-            self.agent_reviewer,
-            self.agent_user_proxy,
-            self.agent_executor,
-        ]
-
-        self.groupchat = autogen.GroupChat(
-            agents=self.agents,
-            messages=[],
-            max_round=50)
-
-        self.manager = autogen.GroupChatManager(
-            groupchat=self.groupchat,
-            llm_config=self.llm_config_gpt3,
+        self.agent_publisher = autogen.AssistantAgent(
+            name="Publisher",
+            system_message="Publisher. You will get the article after reviewer's review, then save the article to a file.",
+            llm_config=self.llm_config_gpt3_pub,
         )
 
         print("[LLMAgentAutoGen] Initialize finished")
@@ -236,13 +289,95 @@ class LLMAgentAutoGen:
     def init_prompt(self, prompt):
         return self
 
-    def run(self, text: str):
-        tokens = self.get_num_tokens(text)
-        print(f"[LLMAgentAutoGen] number of tokens: {tokens}, text: {text}")
+    def collect(self, query: str, work_dir: str):
+        print(f"[LLMAgentAutoGen] query: {query}, work_dir: {work_dir}")
 
-        self.agent_user_proxy.initiate_chat(
-            self.manager,
-            message=text,
+        user_proxy = autogen.UserProxyAgent(
+            name="UserProxy",
+            is_termination_msg=lambda x: x.get("content", "") and "TERMINATE" in "\n".join(x.get("content", "").rstrip().split("\n")[-2:]),
+            human_input_mode="NEVER",
+            max_consecutive_auto_reply=2,
+            code_execution_config={
+                "last_n_messages": 2,
+                "work_dir": work_dir,
+            },
+            llm_config=self.llm_config_gpt3_collection,
+            system_message="A human admin. Interact with the editor to discuss the plan.",
         )
 
-        return self.agent_user_proxy.last_message()["content"]
+        user_proxy.register_function(
+            function_map={
+                "search": search,
+                "scrape": scrape,
+                "arxiv": arxiv_search,
+            }
+        )
+
+        user_proxy.initiate_chat(
+            self.agent_collector,
+            message=query,
+        )
+
+        return user_proxy.last_message()["content"]
+
+    def gen_article(self, query: str, work_dir: str):
+        print(f"[gen_report] query: {query}, work_dir: {work_dir}")
+
+        user_proxy = autogen.UserProxyAgent(
+            name="UserProxy",
+            is_termination_msg=lambda x: x.get("content", "") and "TERMINATE" in "\n".join(x.get("content", "").rstrip().split("\n")[-2:]),
+            human_input_mode="NEVER",
+            max_consecutive_auto_reply=2,
+            code_execution_config=False,
+            llm_config=self.llm_config_gpt3,
+            system_message="A human admin. Interact with the editor to discuss the plan.",
+        )
+
+        agent_executor = autogen.UserProxyAgent(
+            name="Executor",
+            system_message="Executor. Execute the task by provided functions and report the result.",
+            human_input_mode="NEVER",
+            code_execution_config={
+                "last_n_messages": 2,
+                "work_dir": work_dir,
+            },
+        )
+
+        agent_executor.register_function(
+            function_map={
+                "write_to_file": write_to_file,
+            }
+        )
+
+        # create group
+        agents = [
+            user_proxy,
+            agent_executor,
+            self.agent_editor,
+            self.agent_writer,
+            self.agent_reviewer,
+            self.agent_publisher,
+        ]
+
+        groupchat = autogen.GroupChat(
+            agents=agents,
+            messages=[],
+            max_round=50)
+
+        manager = autogen.GroupChatManager(
+            groupchat=groupchat,
+            llm_config=self.llm_config_gpt3,
+        )
+
+        # Set current workdir first
+        os.environ["AN_CURRENT_WORKDIR"] = work_dir
+
+        user_proxy.initiate_chat(
+            manager,
+            message=query,
+        )
+
+        # after group chat finished, the result will be saved to a file
+        article = self.agent_user_proxy.last_message()["content"]
+
+        return article
