a291fe9ec593bf5d6c9c072ae60186a025fb9ed8
Yuzhang Hu <hyzwowtools@gmail.com>
2023-11-24 19:36:15 +0000

    [openai] backward compatible with openai < 1.0

--- src/embedding_openai.py
+++ src/embedding_openai.py
@@ -1,22 +1,28 @@
-import os
-import json
 import openai
 
 from embedding import Embedding
-from db_cli import DBClient
-import utils
+from embedding_openai_0x import EmbeddingOpenAI_0x
+from embedding_openai_1x import EmbeddingOpenAI_1x
 
 
 class EmbeddingOpenAI(Embedding):
     def __init__(self, model_name="openai"):
         super().__init__(model_name)
-        print("Initialized EmbeddingOpenAI")
+
+        self.instance = None
+
+        if openai.__version__ < "1.0.0":
+            self.instance = EmbeddingOpenAI_0x
+        else:  # >= 1.0.0
+            self.instance = EmbeddingOpenAI_1x
+
+        print("Initialized EmbeddingOpenAI Interface")
 
     def dim(self):
-        return 1536
+        return self.instance.dim()
 
     def getname(self, start_date, prefix="news"):
-        return f"{prefix}_embedding__{start_date}".replace("-", "_")
+        return self.instance.getname(start_date, prefix)
 
     def create(
         self,
@@ -24,32 +30,7 @@ class EmbeddingOpenAI(Embedding):
         model_name="text-embedding-ada-002",
         num_retries=3
     ):
-        """
-        It creates the embedding with 1536 dimentions by default
-        """
-        api_key = os.getenv("OPENAI_API_KEY")
-        emb = None
-
-        for i in range(1, num_retries + 1):
-            try:
-                emb = openai.Embedding.create(
-                    input=[text],
-                    api_key=api_key,
-                    model=model_name)
-
-            except openai.error.RateLimitError as e:
-                print(f"[ERROR] RateLimit error during embedding ({i}/{num_retries}): {e}")
-
-                if i == num_retries:
-                    raise
-
-            except openai.error.APIError as e:
-                print(f"[ERROR] Failed during embedding ({i}/{num_retries}): {e}")
-
-                if i == num_retries:
-                    raise
-
-        return emb["data"][0]["embedding"]
+        return self.instance.create(text, model_name, num_retries)
 
     def get_or_create(
         self,
@@ -59,28 +40,5 @@ class EmbeddingOpenAI(Embedding):
         db_client=None,
         key_ttl=86400 * 30
     ):
-        """
-        Get embedding from cache (or create if not exist)
-        """
-        client = db_client or DBClient()
-
-        embedding = client.get_milvus_embedding_item_id(
-            source, page_id)
-
-        if not embedding:
-            # OpenAI embedding model accept 8k tokens, exceed it will
-            # throw exceptions. Here we simply limited it <= 5000 chars
-            # for the input
-
-            EMBEDDING_MAX_LENGTH = int(os.getenv("EMBEDDING_MAX_LENGTH", 5000))
-            embedding = self.create(text[:EMBEDDING_MAX_LENGTH])
-
-            # store embedding into redis (ttl = 1 month)
-            client.set_milvus_embedding_item_id(
-                source, page_id, json.dumps(embedding),
-                expired_time=key_ttl)
-
-        else:
-            embedding = utils.fix_and_parse_json(embedding)
-
-        return embedding
+        return self.instance.get_or_create(
+            text, source, page_id, db_client, key_ttl)
---
+++ src/embedding_openai_1x.py
@@ -0,0 +1,107 @@
+import os
+import json
+import time
+import openai
+from openai import OpenAI
+
+from embedding import Embedding
+from db_cli import DBClient
+import utils
+
+
+class EmbeddingOpenAI_1x(Embedding):
+    """
+    For The implementation for openai < 1.*
+    """
+    def __init__(self, model_name="openai"):
+        super().__init__(model_name)
+        self.client = OpenAI()
+
+        print(f"Initialized EmbeddingOpenAI 1x: {openai.__version__}")
+
+    def dim(self):
+        return 1536
+
+    def getname(self, start_date, prefix="news"):
+        return f"{prefix}_embedding__{start_date}".replace("-", "_")
+
+    def create(
+        self,
+        text: str,
+        model_name="text-embedding-ada-002",
+        num_retries=3
+    ):
+        """
+        It creates the embedding with 1536 dimentions by default
+        """
+        api_key = os.getenv("OPENAI_API_KEY")
+        retry_wait_time = 10  # seconds to wait
+        error_wait_time = 5   # seconds to wait
+        emb = None
+
+        for i in range(1, num_retries + 1):
+            try:
+                emb = self.client.embeddings.create(
+                    input=[text],
+                    api_key=api_key,
+                    model=model_name)
+
+            except openai.RateLimitError as e:
+                print(f"[ERROR] RateLimitError during embedding ({i}/{num_retries}): {e}")
+
+                if i == num_retries:
+                    raise
+
+                time.sleep(retry_wait_time)
+
+            except openai.APITimeoutError as e:
+                print(f"[ERROR] APITimeoutError during embedding ({i}/{num_retries}): {e}")
+
+                if i == num_retries:
+                    raise
+
+                time.sleep(retry_wait_time)
+
+            except openai.APIError as e:
+                print(f"[ERROR] APIError during embedding ({i}/{num_retries}): {e}")
+
+                if i == num_retries:
+                    raise
+
+                time.sleep(error_wait_time)
+
+        return emb["data"][0]["embedding"]
+
+    def get_or_create(
+        self,
+        text: str,
+        source="",
+        page_id="",
+        db_client=None,
+        key_ttl=86400 * 30
+    ):
+        """
+        Get embedding from cache (or create if not exist)
+        """
+        client = db_client or DBClient()
+
+        embedding = client.get_milvus_embedding_item_id(
+            source, page_id)
+
+        if not embedding:
+            # OpenAI embedding model accept 8k tokens, exceed it will
+            # throw exceptions. Here we simply limited it <= 5000 chars
+            # for the input
+
+            EMBEDDING_MAX_LENGTH = int(os.getenv("EMBEDDING_MAX_LENGTH", 5000))
+            embedding = self.create(text[:EMBEDDING_MAX_LENGTH])
+
+            # store embedding into redis (ttl = 1 month)
+            client.set_milvus_embedding_item_id(
+                source, page_id, json.dumps(embedding),
+                expired_time=key_ttl)
+
+        else:
+            embedding = utils.fix_and_parse_json(embedding)
+
+        return embedding
